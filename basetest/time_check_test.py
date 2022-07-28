9adb5a2a3eca0559a9a61a7e6fedb90031d0bbae
dksworld1 <dksworld1@gmail.com>
2022-07-28 22:47:43 +0900

    time_check_test

---
+++ basetest/time_check_test.py
@@ -0,0 +1,41 @@
+import time
+import inspect
+
+def lineno():
+    """    이 함수를 호출한 곳의 라인번호를 리턴한다.    """
+    return inspect.getlineno(inspect.getouterframes(inspect.currentframe())[-1][0])
+start = time.time()
+msg = '{0}, line: {1}, lap-time: {2:.3f}'
+print(msg.format(__file__.split('/')[-1], lineno(), time.time()-start))
+start = time.time()
+aaa = 100
+
+print(msg.format(__file__.split('/')[-1], lineno(), time.time()-start))
+start = time.time()
+
+bbb = 1000
+
+
+
+print(msg.format(__file__.split('/')[-1], lineno(), time.time()-start))
+start = time.time()
+
+
+aaa = 100
+
+print(msg.format(__file__.split('/')[-1], lineno(), time.time()-start))
+start = time.time()
+
+aaa = 100
+
+print(msg.format(__file__.split('/')[-1], lineno(), time.time()-start))
+start = time.time()
+
+
+aaa = 100
+print(msg.format(__file__.split('/')[-1], lineno(), time.time()-start))
+start = time.time()
+
+aaa = 100
+print(msg.format(__file__.split('/')[-1], lineno(), time.time()-start))
+start = time.time()
